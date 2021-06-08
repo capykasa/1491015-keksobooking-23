19e716d4b0508eac5bc4a90c657b889cc5340445
capykasa <capykasa@gmail.com>
2021-06-08 17:41:43 +0500

    Создал функцию и с помощью неё создал 10 объектов

--- js/main.js
+++ js/main.js
@@ -6,14 +6,48 @@ const findRandomIntegerNumber = function (minValue, maxValue) {
   return Math.round(findRandomNumber(minValue, maxValue));
 };
 
-/* пытался взять вдохновение, но взял в итоге весь код отсюда: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random */
-
 const findRandomCoordinates = function (minValue, maxValue, numberAfterPoint) {
   return findRandomNumber(minValue, maxValue).toFixed([numberAfterPoint]);
 };
 
-findRandomNumber(5, 12);
-findRandomIntegerNumber(2, 13);
-findRandomCoordinates(1.110, 1.112, 6);
+/* //////////////////////   ADVERTISEMENTS   ////////////////////////// */
 
-/* воспользовался этим прекрасным методом: https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed */
+const SIMILAR_ADS_COUNT = 10;
+
+const typeHouse = ['palace', 'flat', 'house', 'bungalow', 'hotel'];
+const checkinTime = ['12:00', '13:00', '14:00'];
+const checkoutTime = ['12:00', '13:00', '14:00'];
+const featuresPlace = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const photosPlace = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg', 'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
+
+const createAds = () => {
+  const location = {
+    lat: findRandomCoordinates(35.65000, 35.70000, 5),
+    lng: findRandomCoordinates(139.70000, 139.80000, 5),
+  }
+  return {
+    author: {
+      avatar: 'img/avatars/user' + 0 + findRandomIntegerNumber(1, 8) + '.png',
+    },
+
+    location,
+
+    offer: {
+      title: 'Квартира',
+      adress: location.lat + ', ' + location.lng,
+      price: findRandomIntegerNumber(500, 10000),
+      type: typeHouse[findRandomIntegerNumber(0, typeHouse.length - 1)],
+      rooms: findRandomIntegerNumber(1, 10),
+      guests: findRandomIntegerNumber(1, 10),
+      checkin: checkinTime[findRandomIntegerNumber(0, checkinTime.length - 1)],
+      checkout: checkoutTime[findRandomIntegerNumber(0, checkoutTime.length - 1)],
+      features: featuresPlace.slice(0, findRandomIntegerNumber(1, featuresPlace.length - 1)).join(', '),
+      description: 'Жить можно. Недолго, конечно',
+      photos: photosPlace.slice(0, findRandomIntegerNumber(1, photosPlace.length - 1)).join(', '),
+    },
+  };
+};
+
+const similarAds = new Array(SIMILAR_ADS_COUNT).fill(null).map(() => createAds());
+
+console.log(similarAds);
