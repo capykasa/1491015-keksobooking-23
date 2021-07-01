4681c7428e7cb57ef6f7b44eb08b412ed4734891
capykasa <capykasa@gmail.com>
2021-07-02 00:18:58 +0500

    Добавил мин сумму и синхронизировал время заезда и выезда

--- js/validtion-form.js
+++ js/validtion-form.js
@@ -1,12 +1,24 @@
+import { HOUSES_TYPES } from './create-ads.js';
+
 const MIN_TITLE_LENGTH = 30;
 const MAX_TITLE_LENGTH = 100;
 const MAX_PRICE_VALUE = 1000000;
+let minPriceValue = 1000;
 const titleInput = document.querySelector('#title');
 
 const priceInput = document.querySelector('#price');
 
 const roomNumber = document.querySelector('#room_number');
 const capacity = document.querySelector('#capacity');
+
+const typeOfHouse = document.querySelector('#type');
+
+const timeIn = document.querySelector('#timein');
+const timeOut = document.querySelector('#timeout');
+
+typeOfHouse.addEventListener('change', () => {
+  minPriceValue = HOUSES_TYPES[typeOfHouse.value].minPrice;
+});
 
 const setValidity = (element, item) => {
   element.setCustomValidity(item);
@@ -46,6 +58,16 @@ priceInput.addEventListener('input', () => {
   let message = '';
   if (priceInput.value > MAX_PRICE_VALUE) {
     message = `Максимальное значение: ${MAX_PRICE_VALUE}`;
+  } else if (priceInput.value < minPriceValue) {
+    message = `Минимальное значение: ${minPriceValue}`;
   }
   setValidity(priceInput, message);
+});
+
+timeIn.addEventListener('change', () => {
+  timeOut.value = timeIn.value;
+});
+
+timeOut.addEventListener('change', () => {
+  timeIn.value = timeOut.value;
 });
